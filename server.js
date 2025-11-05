4b86415fc3cc48feeeddad2bb4af90f39516d448
saxdgtea <saxdgtea@gmail.com>
2025-11-06 00:49:38 +0300

    Initial clean commit

--- server.js
+++ server.js
@@ -13,7 +13,12 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://your-frontend-domain.com"]
+    methods: "GET,POST,PUT,DELETE",
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
